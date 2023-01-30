33b10e6454ee3eda33cca33feaa4b184de8aa4ac
zechs <vijay.vky2002@gmail.com>
2023-01-30 21:56:23 +0530

    implemented view binding

--- app/src/main/java/zechs/shrinkme/ui/MainActivity.kt
+++ app/src/main/java/zechs/shrinkme/ui/MainActivity.kt
@@ -3,12 +3,15 @@ package zechs.shrinkme.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import zechs.shrinkme.R
+import zechs.shrinkme.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView( binding.root)
     }
 }
